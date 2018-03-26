3a53c2c1ab22ca46aaa96619c61e3ed717dfcd85
au <cirella_aurelio@yahoo.com>
2018-03-26 15:29:07 +0200

    Impostato riferimenti view.

--- app/src/main/java/com/example/au/myapplication/MainActivity.java
+++ app/src/main/java/com/example/au/myapplication/MainActivity.java
@@ -2,11 +2,29 @@ package com.example.au.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //View layout
+
+    private Button vAumenta;
+    private Button vDiminuisci;
+    private TextView vMessaggio;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //Imposto riferimenti delle view.
+
+        vAumenta = findViewById(R.id.incrementa);
+        vDiminuisci = findViewById(R.id.decrementa);
+        vMessagio = findViewById(R.id.benvenuto);
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
