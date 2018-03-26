396a6cf67d6ad403c0ca8c08bab0a326def459ea
au <cirella_aurelio@yahoo.com>
2018-03-26 15:58:37 +0200

    Non funzionante!

--- app/src/main/java/com/example/au/myapplication/MainActivity.java
+++ app/src/main/java/com/example/au/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.au.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private Button vDiminuisci;
     private TextView vMessaggio;
 
+    private Integer contatore = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,12 +23,45 @@ public class MainActivity extends AppCompatActivity {
 
         vAumenta = findViewById(R.id.incrementa);
         vDiminuisci = findViewById(R.id.decrementa);
-        vMessagio = findViewById(R.id.benvenuto);
-
-
-
+        vMessaggio = findViewById(R.id.benvenuto);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Imposto azioni dei button.
+
+        vAumenta.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                contatore = contatore+1;
+                visualizzaMessaggio();
+            }
+        });
+
+        vDiminuisci.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                contatore = contatore+1;
+                visualizzaMessaggio();
+
+            }
+        });
+
+    }
+
+    /**
+     * Con la sintassi "/**" e invio si crea l'help del metodo. Quando useremo tale metodo compariranno anche i parametri richiesti e di ritorno.
+     */
+
+    private void visualizzaMessaggio()
+    {
+        vMessaggio.setText(getString(R.string.valore) + contatore); //Per inserire un testo prima si inserisce nel file string xml!!!
+
+
+
     }
 }
